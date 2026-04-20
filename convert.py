9f386e3d41e7aa4631fd75c783868d34589f929f
preetham <142284196+preetham976@users.noreply.github.com>
2026-04-20 05:36:21 +0530

    Delete convert.py

--- convert.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Read parquet file
-df = pd.read_parquet("data/cic-collection.parquet")
-
-# Convert to CSV
-df.to_csv("data/dataset.csv", index=False)
-
-print("Conversion completed ✅")
